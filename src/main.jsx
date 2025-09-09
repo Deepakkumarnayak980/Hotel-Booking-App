798f54f8c8378830197f24999a5d4cb56b782281
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-09-09 18:58:55 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import { Button } from "@/components/ui/button"
+
 import App from './app'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <App />
-=======
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
->>>>>>> 9dc862c0c99905eb4c77fc9823c6b879cd7a33ef
   </StrictMode>,
 )
