2c5785b66d952c0bbe0a7f128cd356ba74ca799a
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-09-09 18:10:18 +0530

    add the header layout and same setting

---
+++ src/app/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './home'
+import Header from '@/components/layout/header.layout'
+import Footer from '@/components/layout/footer.layout'
+
+const App = () => {
+  return (
+    <div>
+      
+        <Header />
+        <Home />
+        <Footer/>
+    </div>
+      
+  )
+}
+
+export default App
---
+++ src/components/ui/icon.jsx
@@ -0,0 +1,10 @@
+import icons from "@/lib/icons.jsx";
+
+
+const Icon = ({ icon, ...props }) => {
+  const IconComponent = icons[icon]; 
+  return <IconComponent {...props} />
+};
+
+
+export default Icon
