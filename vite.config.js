94c41e2b31915ca9227370ea1fa88dc55ca89232
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-09-09 18:48:15 +0530

    add some changes

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,17 @@
-<<<<<<< HEAD
-import path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-     "@": path.resolve(__dirname, "./src"),
-    },
-  },
-=======
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-       tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss()
   ],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src")
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
   }
-  
->>>>>>> 9dc862c0c99905eb4c77fc9823c6b879cd7a33ef
 })
