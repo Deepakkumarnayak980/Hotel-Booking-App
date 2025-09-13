f9f88f7ab8c17878848b62846a4d70acc66f5c58
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-09-13 17:24:10 +0530

    add the carousel style page

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Home from './home'
 import Header from '@/components/layout/header.layout'
 import Footer from '@/components/layout/footer.layout'
+import HotelDetails from './hotel-details'
 
 const App = () => {
   return (
     <div>
       
         <Header />
-        <Home />
+        {/* <Home /> */}
+        <HotelDetails />
         <Footer/>
     </div>
       
