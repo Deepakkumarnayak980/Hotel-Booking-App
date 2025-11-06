f654394f5b348436c5a989998d16888b563c4183
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-11-06 23:19:14 +0530

    add the search page

--- src/app/search/filter/index.jsx
+++ src/app/search/filter/index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import StarFilter from './components/star-filter'
 import PriceFilter from './components/price-filter'
 import SortFilter from './components/sort-filter'
+import { Button } from '@/components/ui/button'
 
 const Filter = () => {
   return (
-    <aside>
-        <h3>Filter By:</h3>
+    <aside className='border border-border max-h-max rounded-md w-[240px]'>
+       <div>
+         <h3 className='flex items-center justify-between p-2'>Filter By:</h3>
+         <Button>
+          clear all
+         </Button>
+       </div>
         <StarFilter />
         <PriceFilter />
         <SortFilter />
--- src/app/search/index.jsx
+++ src/app/search/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Filter from './filter'
 import Hotels from './hotels'
+import SortFilter from './filter/components/sort-filter'
+import PaginationFilter from './filter/components/pagination-filter'
 
 const SearchPage = () => {
   return (
-    <div>
+    <div className='container flex gap-4 mt-6 mb-12'>
       <Filter />
-      <Hotels />
+       <section  className='flex-1 space-y-6'>
+        <div className='flex items-center justify-between'>
+          <h1>jaipur:858 properties found</h1>
+          <SortFilter />
+        </div>
+        <Hotels />
+        <PaginationFilter />
+       </section>
     </div>
   )
 }
