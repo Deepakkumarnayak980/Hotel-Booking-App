98dfe3eb32dca3aaac714ef9cc3eabf09a8a57e0
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2025-11-07 19:24:57 +0530

    add the hotel card and some styles through the tailwindCSS

--- src/app/search/hotels/index.jsx
+++ src/app/search/hotels/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import HotelCard from './components/hotel-card';
 
-const Hotels = () => {
+const Hotels = ({ isLoading, data = [], error }) => {
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message || 'Something went wrong'}</p>;
+  if (!data.length) return <p>No hotels found.</p>;
+
   return (
-    <div>Hotels</div>
-  )
-}
+    <div className="space-y-4">
+      {data.map((hotel) => (
+        <HotelCard key={hotel.id} {...hotel} />
+      ))}
+    </div>
+  );
+};
 
-export default Hotels
+export default Hotels;
--- src/app/search/index.jsx
+++ src/app/search/index.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import Filter from './filter'
-import Hotels from './hotels'
-import SortFilter from './filter/components/sort-filter'
-import PaginationFilter from './filter/components/pagination-filter'
+import React from 'react';
+import Filter from './filter';
+import Hotels from './hotels';
+import SortFilter from './filter/components/sort-filter';
+import PaginationFilter from './filter/components/pagination-filter';
+import useQuery from '@/lib/hooks/useQuery';
+import API_CONFIG from '@/config/api.config';
+
 
 const SearchPage = () => {
+  const { data, isLoading, error } = useQuery({
+    url: API_CONFIG.HOTEL.BROWSE_HOTELS,
+    options: {
+      params: {
+        city: 'Delhi',
+        startData: '2025-03-11',
+        endData: '2025-03-13',
+        roomsCount: 2,
+        page: 0,
+        size: 4,
+      },
+    },
+  });
+
+  const hotels = data?.data.content || [];
+
   return (
-    <div className='container flex gap-4 mt-6 mb-12'>
+    <div className="container flex gap-4 mt-6 mb-12">
       <Filter />
-       <section  className='flex-1 space-y-6'>
-        <div className='flex items-center justify-between'>
-          <h1>jaipur:858 properties found</h1>
+      <section className="flex-1 space-y-6">
+        <div className="flex items-center justify-between">
+          <h1 className=" text-xl font-bold">jaipur:858 properties found</h1>
           <SortFilter />
         </div>
-        <Hotels />
-        <PaginationFilter />
-       </section>
-    </div>
-  )
-}
+        <Hotels error={error} isLoading={isLoading} data={hotels} />
 
-export default SearchPage
+        {hotels.length > 0 && <PaginationFilter />}
+      </section>
+    </div>
+  );
+};
+
+export default SearchPage;
